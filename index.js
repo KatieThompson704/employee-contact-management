076f1655083d8f8acefdabeb1cd9fadb42d66a51
Katie <thompsonkatherine704@gmail.com>
2022-10-20 16:17:08 -0400

    created array of questions for user input

--- index.js
+++ index.js
@@ -1,64 +1,114 @@
 const generateHTML = require("./utils/generateHTML");
 const generateCSS = require("./utils/generateCSS");
 
-// TODO: Include packages needed for this application
+// packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [
+// array of questions for user input
+const managerQuestions = [
   {
     type: "input",
-    message: "1. What is the Project Title?",
-    name: "projectTitle",
+    message: "What is the team manager’s name?",
+    name: "managerName",
   },
   {
     type: "input",
-    message: "2. What is the Project Description?",
-    name: "projectdescription",
+    message: "What is the team manager’s id?",
+    name: "managerID",
   },
   {
     type: "input",
-    message: "3. What are the Usage Instructions for this project?",
-    name: "usage",
+    message: "What is the team manger’s e-mail?",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "What is the team manager’s office number?",
+    name: "managerOffice",
   },
   {
     type: "list",
-    message: "4. What License is required for this project? ",
-    choices: [
-      "MIT",
-      "APACHE 2.0",
-      "GPL 3.0",
-      "BSD 3",
-      "None",
-      "Unlicensed",
-      "Other",
-    ],
-    name: "license",
-  },
-  {
-    type: "input",
-    message: "5. What is your github username?",
-    name: "username",
-  },
-  {
-    type: "input",
-    message: "6. What is your email address?",
-    name: "email",
-  },
-  {
-    type: "input",
-    message: "7. What are the installation instructions?",
-    name: "installation",
-  },
-  {
-    type: "input",
-    message: "8. List any contributors to your project: ",
-    name: "contributing",
-  },
-  {
-    type: "input",
-    message: "9. What are the Test instructions for this application?",
-    name: "test",
+    message: "Which type of team member would you like to add?",
+    choices: ["Engineer", "Intern", "Done adding team members."],
+    name: "memberType",
   },
 ];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    message: "What is your engineer’s name?",
+    name: "engineerName",
+  },
+  {
+    type: "input",
+    message: "What is your engineer’s id?",
+    name: "engineerID",
+  },
+  {
+    type: "input",
+    message: "What is your engineer’s e-mail?",
+    name: "engineerEmail",
+  },
+  {
+    type: "input",
+    message: "What is your engineer’s office number?",
+    name: "engineerOffice",
+  },
+  {
+    type: "list",
+    message: "Which type of team member would you like to add?",
+    choices: ["Engineer", "Intern", "Done adding team members."],
+    name: "memberType",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    message: "What is your intern’s name?",
+    name: "internName",
+  },
+  {
+    type: "input",
+    message: "What is your intern's id?",
+    name: "internID",
+  },
+  {
+    type: "input",
+    message: "What is your intern's e-mail?",
+    name: "internEmail",
+  },
+  {
+    type: "input",
+    message: "What is your intern's school?",
+    name: "internOffice",
+  },
+  {
+    type: "list",
+    message: "Which type of team member would you like to add?",
+    choices: ["Engineer", "Intern", "Done adding team members."],
+    name: "memberType",
+  },
+];
+
+function init() {
+  inquirer.prompt(managerQuestions).then((answers) => {
+    fs.writeFileSync("index.html", generateHTML(answers));
+    if ((answers.memberType = "Engineer")) {
+      inquirer.prompt(engineerQuestions).then((answers) => {
+        fs.writeFileSync("index.html", generateHTML(answers));
+      });
+    } else if ((answers.memberType = "Intern")) {
+      inquirer.prompt(internQuestions).then((answers) => {
+        fs.writeFileSync("index.html", generateHTML(answers));
+      });
+    } else {
+    }
+    generateCSS();
+  });
+}
+
+// Function call to initialize app
+init();
